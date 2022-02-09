c07b64b0ea7d9e0f2d8a29aa8b394d4e68582b34
Michael Lobman <michael.lobman@gmail.com>
2022-02-08 21:09:07 -0500

    Refactor code

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -11,11 +11,10 @@ class CheesesController < ApplicationController
     cheese = Cheese.find_by(id: params[:id])
     if cheese
       render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
-   else
-    render json: {error: "cheese not found"}, status: :not_found
-   end
+    else
+      render json: {error: "cheese not found"}, status: :not_found
+    end
      
-   end
   end
 
 end
